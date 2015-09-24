56420753c69861c4ba1700134009c0a8a93c576c
Martin Kolb <admin@vt-learn.de>
2015-09-24 20:09:28 +0200

    Copying text to clipboard before opening browser.

--- DJPreview.py
+++ DJPreview.py
@@ -3,8 +3,6 @@ import sublime, sublime_plugin, webbrowser
 class DjPreviewCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         full_range = sublime.Region(0, self.view.size())
-        txt = self.view.substr(full_range)
-        self.view.insert(edit, 0, txt + txt)
         vt_url = 'http://vt-learn.de/didplanapp/stable/index.html'
+        sublime.set_clipboard(self.view.substr(full_range))
         webbrowser.open(vt_url, 1, True)
-
